a1a5f7e3a0a848320131c2055d2cd13d8e11dc1e
Owen Wassmer <owassmer1@gmail.com>
2024-07-11 16:35:05 -0400

    adjusted api calls

--- app/pitchbook_api.py
+++ app/pitchbook_api.py
@@ -1,5 +1,5 @@
 import requests
-from app.config import PITCHBOOK_API_TOKEN, PITCHBOOK_API_BASE_URL, PITCHBOOK_API_V2_BASE_URL
+from app.config import PITCHBOOK_API_TOKEN, PITCHBOOK_API_BASE_URL
 
 PITCHBOOK_HEADERS = {"Authorization": f"PB-Token {PITCHBOOK_API_TOKEN}"}
 
@@ -23,7 +23,7 @@ def search_investors(company_id):
     return []
 
 def get_company_bio(company_id):
-    url = f"{PITCHBOOK_API_V2_BASE_URL}/companies/{company_id}/bio"
+    url = f"{PITCHBOOK_API_BASE_URL}/companies/{company_id}/bio"
     response = requests.get(url, headers=PITCHBOOK_HEADERS)
     if response.status_code == 200:
         return response.json()
