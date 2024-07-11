fdb629cb322b7dece481b1939837ec82e8aebe5d
Owen Wassmer <owassmer1@gmail.com>
2024-07-11 16:53:02 -0400

    fixed api calls

--- app/config.py
+++ app/config.py
@@ -7,4 +7,5 @@ SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 SLACK_CHANNEL_ID = os.getenv("SLACK_CHANNEL_ID")
 PITCHBOOK_API_TOKEN = os.getenv("PITCHBOOK_API_TOKEN")
-PITCHBOOK_API_BASE_URL = "https://api.pitchbook.com"
+PITCHBOOK_API_BASE_URL = "https://api.pitchbook.com/v1"
+PITCHBOOK_API_BASE_URL_V2 = "https://api-v2.pitchbook.com"
