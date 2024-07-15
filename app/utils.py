114c02b3f84792502e59332f98e578918f750104
owenwassmer <owen@qedinvestors.com>
2024-07-15 11:34:21 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -4,13 +4,15 @@ from app.logger import logger
 
 def extract_domain(url):
   # Define a regular expression pattern for extracting the domain
+  urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', url)
   pattern = r"(https?://)?(www\d?\.)?(?P<domain>[\w\.-]+\.\w+)(/\S*)?"
   # Use re.match to search for the pattern at the beginning of the URL
-  match = re.match(pattern, url)
+  if urls[0]:
+    match = re.match(pattern, urls[0])
   # Check if a match is found
-  if match:
+    if match:
   # Extract the domain from the named group "domain"
-    domain = match.group("domain")
-    return domain
+      domain = match.group("domain")
+      return domain
   else:
     return None
