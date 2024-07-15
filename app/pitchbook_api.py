9763ba1ca0d1ef80870e77bc09ac3585adf910a5
owenwassmer <owen@qedinvestors.com>
2024-07-15 11:44:49 -0400

    Update pitchbook_api.py

--- app/pitchbook_api.py
+++ app/pitchbook_api.py
@@ -23,7 +23,7 @@ def search_investors(company_id):
     return []
 
 def get_company_bio(company_id):
-    url = f"{PITCHBOOK_API_BASE_URL}companies/{company_id}/bio"
+    url = f"{PITCHBOOK_API_BASE_URL_V2}companies/{company_id}/bio"
     response = requests.get(url, headers=PITCHBOOK_HEADERS)
     if response.status_code == 200:
         return response.json()
